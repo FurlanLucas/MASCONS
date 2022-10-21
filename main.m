b13c877efc8b253076874a1c1c4a9d5df42c4191
FurlanLucas <84166961+FurlanLucas@users.noreply.github.com>
2022-10-21 19:31:54 +0200

    Update main.m
    Figure update.

--- main.m
+++ main.m
@@ -1,77 +1,82 @@
-clear; close all; tic;
+clear; tic;
 %% Analysis settings
-global figNumber figDir; 
+
+global figNumber; figNumber = 1;
+
 bodyName = "216 Kleopatra"; % Name of the body (by the input file)
-numberOfDiv = 5; % Number of MASCONS par tetrahedral;
+numberOfDivisions = 5; % Gauss integration order;
+spacePoints = 100; % Points in space;
+order_spacePoints = 2; % Order of polynomial spacing points;
 
-figDir = pwd + "\fig\";
-if ~isfolder('fig')
-    mkdir(pwd + "\fig")
-end
+% Analysis name
+analysisName = bodyName + "_d" + num2str(numberOfDivisions) + "_p" + ...
+    num2str(spacePoints) + "_o" + num2str(order_spacePoints);
 
-figNumber = 1;
+% Creates the directory if it doesn't exist
+figDir = pwd + "\fig\MASCONS_" + analysisName; checkDir(figDir);
+varDir = pwd + "\var"; checkDir(varDir);
 
 %% Main calculations
-fprintf('Stating analysis for %s body.\n', bodyName);
+fprintf('\nStating MASCONS analysis for %s body.\n', bodyName);
 file = importdata(bodyName + ".txt");
 
 % Get the number of vertices
-body = struct();
-    body.vertices = file.data(cell2mat(file.textdata)=='v',:);
-    body.faces = file.data(cell2mat(file.textdata)=='f',:);
-    body.length = length(body.faces);
+vertices = file.data(cell2mat(file.textdata)=='v',:);
+faces = file.data(cell2mat(file.textdata)=='f',:);
     
 % Figura do corpo
 figure(figNumber);
-patch('faces', body.faces, 'vertices', body.vertices, ...
-    'EdgeColor','k','FaceColor','none');
-axis equal; view(30,10);
-saveas(figure(figNumber), figDir + "bodyDescription.png");
+patch('faces', faces, 'vertices', vertices, 'EdgeColor', 'k', ...
+    'FaceColor','none'); axis equal; view(30,10);
+saveas(figure(figNumber), figDir + "\bodyDescription.png");
 figNumber = figNumber + 1;
 
 disp('Analysis settings:')
-fprintf('\tFaces number: %d\n', length(body.faces));
-fprintf('\tVertices number: %d\n', length(body.vertices));
+fprintf('\tFaces number: %d\n', length(faces));
+fprintf('\tVertices number: %d\n', length(vertices));
 
-% Building MASCONS data
-MSC = getMASCONS(body, 3);
+%% Potencial calculus
+MSC = getMASCONS(faces, vertices, numberOfDivisions, figDir);
 fprintf('\tMASCONS number: %d\n', length(MSC.centers));
 
+% Points to be calculated the potencial
+x = polySpaced(-252, 252, 0, spacePoints, order_spacePoints);
+y = polySpaced(-252, 252, 0, spacePoints, order_spacePoints);
+z = polySpaced(-252, 252, 0, spacePoints, order_spacePoints);
 
-%% Potencial
-x = polySpaced(-252, 252, 0, 400, 5);
-y = polySpaced(-252, 252, 0, 400, 5);
-z = 0;
-U = getPotencial(MSC, x, y, z);
-[X, Y] = meshgrid(x, y);
+% Get the potential
+[U, ~] = getPotencial(MSC, x, y, z);
 
 % Plot potential data
-figure(3); 
-surf(X, Y, U, 'EdgeColor', 'none');
-colorbar(); xlabel('X [km]'); ylabel('Y [km]'); zlabel('U');
-view(60, 35);
+figure(figNumber);  [X, Y] = meshgrid(x, y);
+surf(X, Y, U(:, :, floor(length(z)/2)), 'EdgeColor', 'none'); colorbar(); 
+xlabel('X [km]'); ylabel('Y [km]'); zlabel('U'); view(60, 35);
+saveas(figure(figNumber), figDir + "\potential.png");
+figNumber = figNumber + 1;
 
 %% Ending
 s = toc;
-fprintf('Code ended successfully');
+fprintf('Code ended successfully.\n');
 fprintf('Total execution time: %dmin e %.2fs.\n', floor(s/60), ...
     s - floor(s/60)*60);
-%close all;
+save(varDir + "\MASCONS_" + analysisName, 'U', 'x', 'y', 'z', ...
+    'bodyName', 's');
 
 %% My functions
-function MSC = getMASCONS(body, numberOfDivisions)
+function MSC = getMASCONS(faces, vertices, numberOfDivisions, figDir)
     % Function responsable for getting the centroids of each MASCONS and
     % their respective volume. The only input is the body description, un
     % struct with the filds 'vertices' and 'faces'.
     
     % Inputs
-    global figNumber figDir;
+    global figNumber;
     
     if ~exist('numberOfDivisions', 'var')
         numberOfDivisions = 1;
     end
     
-    L = numberOfDivisions*body.length;
+    len = length(faces);
+    L = numberOfDivisions*len;
     MSC = struct();
         MSC.centers = zeros(3, L);
         MSC.volume = zeros(1, L);
@@ -80,32 +85,32 @@ function MSC = getMASCONS(body, numberOfDivisions)
     figure(figNumber); hold on;
     
     % General calculations
-    A = body.vertices(body.faces(:,1), :)'/numberOfDivisions;
-    B = body.vertices(body.faces(:,2), :)'/numberOfDivisions;
-    C = body.vertices(body.faces(:,3), :)'/numberOfDivisions;
+    A = vertices(faces(:,1), :)'/numberOfDivisions;
+    B = vertices(faces(:,2), :)'/numberOfDivisions;
+    C = vertices(faces(:,3), :)'/numberOfDivisions;
     
-    MSC.centers(:, 1:body.length) = ...
-        (A + B + C + zeros(3, body.length))/4;
-    MSC.volume(1:body.length) = dot(C, cross(A, B))/6;
+    MSC.centers(:, 1:len) = ...
+        (A + B + C + zeros(3, len))/4;
+    MSC.volume(1:len) = dot(C, cross(A, B))/6;
     
     plot3(MSC.centers(1,:), MSC.centers(2,:), MSC.centers(3,:), 'o', ...
         'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k', 'MarkerSize', 0.8);
     
     for i = 2:numberOfDivisions
-        Anew = body.vertices(body.faces(:,1), :)'*i/numberOfDivisions;
-        Bnew = body.vertices(body.faces(:,2), :)'*i/numberOfDivisions;
-        Cnew = body.vertices(body.faces(:,3), :)'*i/numberOfDivisions;
+        Anew = vertices(faces(:,1), :)'*i/numberOfDivisions;
+        Bnew = vertices(faces(:,2), :)'*i/numberOfDivisions;
+        Cnew = vertices(faces(:,3), :)'*i/numberOfDivisions;
         
-        MSC.centers(:, (1:body.length) + (i-1)*L) = ...
+        MSC.centers(:, (1:len) + (i-1)*L) = ...
             (A + B + C + Anew + Bnew + Cnew)/6;
         
-        MSC.volume(:, (1:body.length) + (i-1)*L) = ...
+        MSC.volume(:, (1:len) + (i-1)*L) = ...
             dot(Cnew, cross(Anew, Bnew))/6 - dot(C, cross(A, B))/6;
         
         % Plot the MASCONS centers
-        plot3(MSC.centers(1, (1:body.length) + (i-1)*L), ...
-              MSC.centers(2, (1:body.length) + (i-1)*L), ...
-              MSC.centers(3, (1:body.length) + (i-1)*L), 'o', ...
+        plot3(MSC.centers(1, (1:len) + (i-1)*L), ...
+              MSC.centers(2, (1:len) + (i-1)*L), ...
+              MSC.centers(3, (1:len) + (i-1)*L), 'o', ...
               'MarkerFaceColor', colors(i-1), 'MarkerEdgeColor', ...
               colors(i-1), 'MarkerSize', 0.8);
         
@@ -115,31 +120,37 @@ function MSC = getMASCONS(body, numberOfDivisions)
     % Plots
     
     axis equal; view(30,10);
-    saveas(figure(figNumber), figDir + "MASCONS.png");
+    saveas(figure(figNumber), figDir + "\MASCONS.png");
     figNumber = figNumber + 1;
     
 end
 
-function U = getPotencial(MSC, X, Y, Z)
+function [U, Ur] = getPotencial(MSC, X, Y, Z)
     % Function to get the total potential of the body at a poit (x, y, z).
     % If the points ar vectors, the potencial are evaluated at each point.
     
     G = 6.67259e-20; % [m³/kg*s] Gravitational constant;
     rho = 4270*1e9; % [kg/m³] Mean body density;
     U = zeros(length(X), length(Y), length(Z));
-    
+    Ur = zeros(length(X), length(Y), length(Z));
+    fprintf('\n');
     % For loop through the inputs points
+    count = 1; total = length(X)*length(Y)*length(Z);
     for i = 1:length(X)
         for j = 1:length(Y)
             for k = 1:length(Z)
                 % Potencial for each MASCONS divided by G, saved as vector
                 r = [X(i); Y(j); Z(k)];
                 Uu = MSC.volume*rho ./ sqrt(sum((MSC.centers - r).^2));
+                Uur = MSC.volume*rho ./ (sum((MSC.centers - r).^2));
 
                 % Total potencial (sum of each MASCONS potencial)
                 U(i, j, k) = -G*sum(Uu);
+                Ur(i, j, k) = G*sum(Uur);
+                count = count + 1;
             end
         end
+        fprintf("\t\tProgress: %4.2f %%\n", count*100/total);
     end    
 end
 
@@ -157,4 +168,10 @@ function out = polySpaced(startP, endP, peakP, pointsNumber, exp)
     aux2 = ceil(pointsNumber/2) - 1;
     out = [peakP + (startP - peakP)*((aux1-(0:aux1))/aux1).^exp, ...
            peakP + (endP - peakP)*((0:aux2)/aux2).^exp];
+end
+
+function checkDir(nameDir)
+    if ~isfolder(nameDir)
+        mkdir(nameDir)
+    end
 end
